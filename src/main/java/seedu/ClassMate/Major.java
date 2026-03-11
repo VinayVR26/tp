2127cf075a03cee018a71020a8f8a00fa9c59f96
wn <lauweina@gmail.com>
2026-03-11 18:49:59 +0800

    Add comment to clarify how to print moduleList to view the list of CEG modules

--- src/main/java/seedu/ClassMate/Major.java
+++ src/main/java/seedu/ClassMate/Major.java
@@ -14,6 +14,7 @@ public class Major {
         setupCEGModules();
     }
 
+    // Print moduleList using cegMajor.toString()
     @Override
     public String toString() {
         if (coreModules.isEmpty()) {
