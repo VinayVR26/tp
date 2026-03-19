3d42b76c594e38ad198c62fcbf6338d055ebdf1e
wn <lauweina@gmail.com>
2026-03-20 00:21:08 +0800

    Adapt HelpCommand to use new ui.showHelp()

--- src/main/java/seedu/classmate/Parser.java
+++ src/main/java/seedu/classmate/Parser.java
@@ -8,7 +8,6 @@ import seedu.classmate.commands.Command;
  * Based on the command word, the corresponding {@code Command} is instantiated.
  */
 public class Parser {
-    static Ui ui;
 
     /**
      * Parses the user's input and returns the corresponding {@code Command}.
@@ -32,21 +31,6 @@ public class Parser {
 
         String commandWord = components[0].toLowerCase();
         String arguments = (components.length > 1) ? components[1].trim().toUpperCase() : "";
-
-        switch (commandWord) {
-        case "help":
-            ui.showHelp();
-            break;
-
-        case "bye":
-            ui.showGoodbye();
-            break;
-
-        // TODO: add more cases
-
-        default:
-            throw new ClassMateException("Unknown command. Enter 'help for list of commands.");
-        }
 
         return CommandManager.createCommand(commandWord, arguments);
     }
